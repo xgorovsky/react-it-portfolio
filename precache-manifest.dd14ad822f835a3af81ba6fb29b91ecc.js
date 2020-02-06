d22dc3c72bff42570358fbecae2fef38d9665ddd
xgorovsky <igorbiedrawa@gmail.com>
2020-02-06 01:45:53 +0100

    Updates

---
+++ precache-manifest.dd14ad822f835a3af81ba6fb29b91ecc.js
@@ -0,0 +1,74 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f98b3664d64248d5348e1313eee1e269",
+    "url": "/react-it-portfolio/index.html"
+  },
+  {
+    "revision": "bc374e18350e75de1696",
+    "url": "/react-it-portfolio/static/css/3.122b008c.chunk.css"
+  },
+  {
+    "revision": "491eb52414befd4e3b41",
+    "url": "/react-it-portfolio/static/css/main.e1cc8266.chunk.css"
+  },
+  {
+    "revision": "9d67e31e3c1d71cf1434",
+    "url": "/react-it-portfolio/static/js/2.bf227608.chunk.js"
+  },
+  {
+    "revision": "f4a1bf76af436fc3c382b19742522e6a",
+    "url": "/react-it-portfolio/static/js/2.bf227608.chunk.js.LICENSE"
+  },
+  {
+    "revision": "bc374e18350e75de1696",
+    "url": "/react-it-portfolio/static/js/3.a4b62ec6.chunk.js"
+  },
+  {
+    "revision": "cac60acce0027fd61c30",
+    "url": "/react-it-portfolio/static/js/4.f7ca8c11.chunk.js"
+  },
+  {
+    "revision": "3fc7a8afc337456e6eb73316df3df069",
+    "url": "/react-it-portfolio/static/js/4.f7ca8c11.chunk.js.LICENSE"
+  },
+  {
+    "revision": "491eb52414befd4e3b41",
+    "url": "/react-it-portfolio/static/js/main.d6f03076.chunk.js"
+  },
+  {
+    "revision": "d38ecebd6f648b302e43",
+    "url": "/react-it-portfolio/static/js/runtime-main.9dbc16c1.js"
+  },
+  {
+    "revision": "841f8ea2b671cc5f55bd892da0abcbb4",
+    "url": "/react-it-portfolio/static/media/iblogo.841f8ea2.svg"
+  },
+  {
+    "revision": "1947ba095401011553dd830916edf53a",
+    "url": "/react-it-portfolio/static/media/main.1947ba09.svg"
+  },
+  {
+    "revision": "7f5febd41e0c340ea100daa3a21bb7bc",
+    "url": "/react-it-portfolio/static/media/moneyapp.7f5febd4.jpg"
+  },
+  {
+    "revision": "690cc256d3ed7093ea830e79e57eb9b7",
+    "url": "/react-it-portfolio/static/media/myPhoto.690cc256.jpg"
+  },
+  {
+    "revision": "3f30e0b1af421f4a71e75103c8cfd61e",
+    "url": "/react-it-portfolio/static/media/portfolio.3f30e0b1.jpg"
+  },
+  {
+    "revision": "9cfe5dec1d9602386082820daf0004eb",
+    "url": "/react-it-portfolio/static/media/recipeapp.9cfe5dec.jpg"
+  },
+  {
+    "revision": "25f9f8e4260d1d0930960351322dca5f",
+    "url": "/react-it-portfolio/static/media/weather2app.25f9f8e4.jpg"
+  },
+  {
+    "revision": "e277ab5d3e70238d30a01c5121ddeda4",
+    "url": "/react-it-portfolio/static/media/weatherapp.e277ab5d.jpg"
+  }
+]);
